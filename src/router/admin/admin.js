bcf44e42bf70b221672bb8b8f26fb0122ebafde9
zly <906005454@qq.com>
2019-10-21 10:09:19 +0800

    more

--- src/router/admin/admin.js
+++ src/router/admin/admin.js
@@ -10,44 +10,44 @@ const adminRouter = {
   meta: {
     title: '信息维护',
     icon: 'edit',
-    roles:[1,4,7]
+   
   },
   children: [
     {
       path: 'department',
       component: () => import('@/views/admin/department'),
       name: 'department',
-      meta: { title: '科室管理', noCache: true ,roles:[1,7]}
+      meta: { title: '科室管理', noCache: true}
     },
     {
       path: 'registLevel',
       component: () => import('@/views/admin/registLevel'),
       name: 'registLevel',
-      meta: { title: '挂号级别管理', noCache: true ,roles:[1,7]}
+      meta: { title: '挂号级别管理', noCache: true}
     },
     {
       path: 'settleCat',
       component: () => import('@/views/admin/settleCat'),
       name: 'settleCat',
-      meta: { title: '结算类别管理', noCache: true ,roles:[1,7]}
+      meta: { title: '结算类别管理', noCache: true }
     },
     {
       path: 'diagnosis',
       component: () => import('@/views/admin/diagnosis'),
       name: 'diagnosis',
-      meta: { title: '诊断目录管理', noCache: true ,roles:[1,7]}
+      meta: { title: '诊断目录管理', noCache: true }
     },
     {
       path: 'non_drug',
       component: () => import('@/views/admin/non_drug'),
       name: 'non_drug',
-      meta: { title: '非药品收费项目管理', noCache: true,roles:[1,7] }
+      meta: { title: '非药品收费项目管理', noCache: true }
     },
     {
       path: 'drug',
       component: () => import('@/views/admin/drug'),
       name: 'drug',
-      meta: { title: '药品管理', noCache: true ,roles:[1,4,7]}
+      meta: { title: '药品管理', noCache: true }
     }
   ]
 }
